f70bdb727491159195d8f652fd846b390159070d
Golta Haghighi <goltaahaghighi@gamil.com>
2021-10-19 13:31:03 +0330

    sqrt project is ready update

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const userInput = prompt("Enter a number :");
-for (let i = 1 ; i < userInput ; i++){
+for (let i = 1 ; i <= userInput ; i++){
     let root = Math.sqrt(i);
     if (Math.floor(root) === root) {
          console.log(`sqrt of ${Math.pow(root,2)} is ${root}`);
